48b825bf105445d81c91acd697c8bd6aafadb699
yare1 <129279812+yare1@users.noreply.github.com>
2025-02-23 05:24:34 -0600

    tareas

---
+++ Views/js/scriptsta.js
@@ -0,0 +1,4 @@
+document.getElementById("file").addEventListener("change", function() {
+    var fileName = this.files.length > 0 ? this.files[0].name : " ";
+    document.getElementById("nombrea").textContent = fileName;
+});
